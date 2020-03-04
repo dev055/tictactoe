936ea939a5389157bae355427f9a4728eadd3ea2
dev dev <dev.dev.055.dev@gmail.com>
2020-03-04 20:27:19 +0100

    feature: add packages for ui and models

--- app/src/main/java/com/example/tictactoe/ui/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.tictactoe
+package com.example.tictactoe.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.tictactoe.R
 
 class MainActivity : AppCompatActivity() {
 
