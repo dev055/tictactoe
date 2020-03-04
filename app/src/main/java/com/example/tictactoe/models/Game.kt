cc18bddab692b108a860368d82ed9a1d85829700
dev dev <dev.dev.055.dev@gmail.com>
2020-03-04 20:39:18 +0100

    feature: add a board to Game model

--- app/src/main/java/com/example/tictactoe/models/Game.kt
+++ app/src/main/java/com/example/tictactoe/models/Game.kt
@@ -1,5 +1,8 @@
 package com.example.tictactoe.models
 
 class Game() {
-
+    lateinit var board: HashMap<Int,String>
+    init {
+        board = HashMap()
+    }
 }
--- app/src/test/java/com/example/tictactoe/GameTest.kt
+++ app/src/test/java/com/example/tictactoe/GameTest.kt
@@ -2,13 +2,24 @@ package com.example.tictactoe
 
 import com.example.tictactoe.models.Game
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 class GameTest {
+    private lateinit var game: Game
+
+    @Before
+    fun instantiateGame() {
+        game = Game()
+    }
 
     @Test
     fun testIfGameExists() {
-        Assert.assertEquals(true, Game() != null)
+        Assert.assertEquals(true, game != null)
     }
 
+    @Test
+    fun testIfGameHasABoard() {
+        Assert.assertEquals(true,game.board != null)
+    }
 }
