394304c3dcd161c80911fd4ef875e84ee9b45f46
dev dev <dev.dev.055.dev@gmail.com>
2020-03-05 21:40:51 +0100

    feature: add function to set player name

--- app/src/test/java/com/example/tictactoe/GameManagerTest.kt
+++ app/src/test/java/com/example/tictactoe/GameManagerTest.kt
@@ -2,6 +2,9 @@ package com.example.tictactoe
 
 import com.example.tictactoe.models.Game
 import com.example.tictactoe.models.GameManagerForTesting
+import com.example.tictactoe.models.Player
+import com.example.tictactoe.models.Type
+import com.example.tictactoe.utils.PlayerNameUtil
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
@@ -17,5 +20,22 @@ class GameManagerTest {
     @Test
     fun testIfGameManagerContainsGame() {
         Assert.assertEquals(true,gameManagerForTesting.mGame != null)
+    }
+
+    @Test
+    fun testIfGameHasPlayerFromName() {
+        gameManagerForTesting.mGame!!.mPlayerOne = Player("player 1",Type.TYPE_X)
+        gameManagerForTesting.mGame!!.mPlayerTwo = Player("player 2",Type.TYPE_O)
+        val condition = (gameManagerForTesting.mGame!!.mPlayerOne != null && gameManagerForTesting.mGame!!.mPlayerTwo != null)
+        Assert.assertEquals(true,condition)
+    }
+
+    @Test
+    fun testCreatingPlayerFromEmptyName() {
+        val playerOne = PlayerNameUtil.defaultName("")
+        val playerTwo = PlayerNameUtil.defaultName("")
+        gameManagerForTesting.mGame!!.mPlayerOne = Player(playerOne,Type.TYPE_X)
+        gameManagerForTesting.mGame!!.mPlayerTwo = Player(playerTwo,Type.TYPE_O)
+        Assert.assertEquals(true,gameManagerForTesting.mGame!!.mPlayerTwo!!.name == "player 2")
     }
 }
