681facf0e3acd97f1676d28e91964df1cd498ca7
dev dev <dev.dev.055.dev@gmail.com>
2020-03-05 22:08:53 +0100

    feature: implement logic for listeenrs

--- app/src/main/java/com/example/tictactoe/managers/GameManager.kt
+++ app/src/main/java/com/example/tictactoe/managers/GameManager.kt
@@ -1,15 +1,22 @@
 package com.example.tictactoe.managers
 
+import com.example.tictactoe.listeners.IViewListener
 import com.example.tictactoe.models.Game
 import com.example.tictactoe.models.Player
 import com.example.tictactoe.models.Type
 
 object GameManager {
     private var mGame: Game? = null
+    private var mListeners = ArrayList<IViewListener>()
+
+    fun getGame(): Game? = mGame
+
+    fun addListener(listener: IViewListener) { mListeners.add(listener) }
 
     fun createPlayer(playerOneName: String, playerTwoName: String) {
         mGame = Game()
         mGame!!.mPlayerOne = Player(playerOneName,Type.TYPE_X)
         mGame!!.mPlayerTwo = Player(playerTwoName,Type.TYPE_O)
+        mListeners.forEach { listener -> listener.goNextView() }
     }
 }
