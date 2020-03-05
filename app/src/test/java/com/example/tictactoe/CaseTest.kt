08fd164800492e49d6029dd5d3c19bffe4724134
dev dev <dev.dev.055.dev@gmail.com>
2020-03-06 00:28:38 +0100

    refactor: write test for property type

--- app/src/test/java/com/example/tictactoe/CaseTest.kt
+++ app/src/test/java/com/example/tictactoe/CaseTest.kt
@@ -1,6 +1,7 @@
 package com.example.tictactoe
 
 import com.example.tictactoe.models.Case
+import com.example.tictactoe.models.Type
 import org.junit.Assert
 import org.junit.Test
 
@@ -13,6 +14,8 @@ class CaseTest {
 
     @Test
     fun testCaseHasAType() {
-
+        val case = Case()
+        case.type = Type.TYPE_O
+        Assert.assertEquals(true, case.type != null)
     }
 }
