5ae8b49b5a785f4242478c8c7f38b576b4a839fa
dev dev <dev.dev.055.dev@gmail.com>
2020-03-06 16:53:47 +0100

    refactor: add listener for dialog

--- app/src/main/java/com/example/tictactoe/managers/GameManager.kt
+++ app/src/main/java/com/example/tictactoe/managers/GameManager.kt
@@ -1,5 +1,6 @@
 package com.example.tictactoe.managers
 
+import com.example.tictactoe.listeners.IDialogListener
 import com.example.tictactoe.listeners.IViewListener
 import com.example.tictactoe.models.Game
 import com.example.tictactoe.models.Player
@@ -9,15 +10,21 @@ object GameManager {
     private var number = 3
     private var mGame: Game? = null
     private var mListeners = ArrayList<IViewListener>()
+    private var mDialogListeners = ArrayList<IDialogListener>()
 
     fun getGame(): Game? = mGame
 
     fun addListener(listener: IViewListener) { mListeners.add(listener) }
+    fun addDialogListener(listener: IDialogListener) { mDialogListeners.add(listener) }
 
     fun createPlayer(playerOneName: String, playerTwoName: String) {
         mGame = Game(number, number)
         mGame!!.mPlayerOne = Player(playerOneName,Type.TYPE_X)
         mGame!!.mPlayerTwo = Player(playerTwoName,Type.TYPE_O)
         mListeners.forEach { listener -> listener.goNextView() }
+    }
+
+    fun endOfGame() {
+        mDialogListeners.forEach { listener -> listener.show() }
     }
 }
